7e69fde16b88263e880d36b20acb0830e1e167de
“First <Kerger06@aol.com>
2020-08-20 12:52:38 -0500

    added key press animation

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -7,7 +7,8 @@ var userClickedPattern = [];
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-})
+    playSound(userChosenColour);
+});
 
 
 function nextSequence() {
@@ -16,7 +17,17 @@ function nextSequence() {
     gamePattern.push(randomChosenColour)
 
 $("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}    
 
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-audio.play()
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play()
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
 }
