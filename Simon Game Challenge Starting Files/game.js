33d27a0fac6101edb957ee088c75bb7741f8a8d8
“First <Kerger06@aol.com>
2020-08-20 12:32:39 -0500

    added game sounds to pair with selected random color

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,8 +4,12 @@ var buttonColours = ["red", "blue", "green", "yellow"]
 
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random()*3);
+    var randomNumber = Math.floor(Math.random()* 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour)
-    console.log(gamePattern)
+
+$("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+audio.play()
 }
