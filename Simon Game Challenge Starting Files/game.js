1a10d5099add22a5157da51e17be997e2f51592e
“First <Kerger06@aol.com>
2020-08-20 13:25:47 -0500

    completed project

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -27,22 +27,20 @@ $(".btn").click(function() {
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        console.log("success");
         if (userClickedPattern.length === gamePattern.length){
             setTimeout(function () {
                 nextSequence();
             }, 1000);
         }
     } else {
-        console.log("wrong");
-
         playSound("wrong");
         $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
         setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
 
-        $("level-title").text("Game Over, Press Any Key to Restart");
 
         startOver()
     }
@@ -53,12 +51,12 @@ function nextSequence() {
     userClickedPattern = [];
 
     level++;
-
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random()* 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour)
 
-$("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }    
 
