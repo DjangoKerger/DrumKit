9d3769de0167a8762ee8501c1140751ea19b2c3d
“First <Kerger06@aol.com>
2020-08-20 13:18:56 -0500

    start over function added, resets level and game pattern

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -42,7 +42,9 @@ function checkAnswer(currentLevel) {
             $("body").removeClass("game-over");
         }, 200);
 
-        $("level-title").text("Game Over, Press Any Key to Restart")
+        $("level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver()
     }
 }
 
@@ -70,4 +72,10 @@ function animatePress(currentColor) {
     setTimeout(function () {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
